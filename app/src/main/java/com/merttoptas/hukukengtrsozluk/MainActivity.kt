ff88a78f2cf2465f1f38f47429bf3153d0fff5c0
merttoptas <merttoptas34@gmail.com>
2019-12-01 22:22:06 +0300

    fragment added.

--- app/src/main/java/com/merttoptas/hukukengtrsozluk/MainActivity.kt
+++ app/src/main/java/com/merttoptas/hukukengtrsozluk/MainActivity.kt
@@ -14,18 +14,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         lateinit var bottomNavigationView: BottomNavigationView
+        var fragmentId :String
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val typeface = Typeface.createFromAsset(assets, "fonts/SourceSansPro-Regular.ttf")
-
-        tv_law.typeface = typeface
-        tv_law_subtitle.typeface = typeface
-
-        supportActionBar!!.hide()
-        val btnAnimation : Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.button_animation)
-
+        setTypeFace()
 
         cardView_words_learn.setOnClickListener {
 
@@ -34,33 +28,42 @@ class MainActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
-
-    }
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
-            menuItem ->
-        when (menuItem.itemId){
-            R.id.navigationHome -> {
-                //val fragment = BlogFragment()
-          //      supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
-          //          .commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigationFavorites -> {
-                return@OnNavigationItemSelectedListener true
-            }
-
-            R.id.navigationSearch -> {
-                return@OnNavigationItemSelectedListener true
-            }
-
-            R.id.navigationSettings -> {
-                return@OnNavigationItemSelectedListener true
-            }
-
+        cardView_words.setOnClickListener{
+            val fragmentSearch = SearchFragment()
+            fragmentId = fragmentSearch.toString()
+            val intent = Intent(this, WordsActivity::class.java)
+            intent.putExtra("fragmentSearch", fragmentId)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
         }
-        false
+
+        cardview_favorite.setOnClickListener{
+            val fragmentFavorite = FavoriteFragment()
+            fragmentId = fragmentFavorite.toString()
+            val intent = Intent(this, WordsActivity::class.java)
+            intent.putExtra("fragmentFavorite", fragmentId)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            finish()
+        }
+        cardview_about.setOnClickListener {
+            val fragmentAbout = AboutUsFragment()
+            fragmentId = fragmentAbout.toString()
+            val intent = Intent(this, WordsActivity::class.java)
+            intent.putExtra("fragmentAboutUs", fragmentId)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+            finish()
+        }
 
     }
 
+    fun  setTypeFace(){
+        val typeface = Typeface.createFromAsset(assets, "fonts/SourceSansPro-Regular.ttf")
+        tv_law.typeface = typeface
+        tv_law_subtitle.typeface = typeface
+    }
 }
--- app/src/main/java/com/merttoptas/hukukengtrsozluk/WordsActivity.kt
+++ app/src/main/java/com/merttoptas/hukukengtrsozluk/WordsActivity.kt
@@ -1,12 +1,77 @@
+@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
 package com.merttoptas.hukukengtrsozluk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.graphics.toColorInt
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_words_activitiy.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.app_bar_main.toolbar
 
 class WordsActivity : AppCompatActivity() {
 
+    lateinit var searchFragment : SearchFragment
+    lateinit var  fm : FragmentManager
+    lateinit  var fragmentid : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_words_activitiy)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = "Hukuk Sözlüğü"
+        supportActionBar!!.setDefaultDisplayHomeAsUpEnabled(true)
+        navigation.setOnNavigationItemSelectedListener (mOnNavigationItemSelectedListener)
+
+        when {
+            intent.hasExtra("fragmentFavorite") -> {
+                fragmentid = intent.getStringExtra("fragmentFavorite")
+
+                navigation.selectedItemId = R.id.navigationFavorites
+
+            }
+            intent.hasExtra("fragmentSearch") -> {
+                fragmentid = intent.getStringExtra("fragmentSearch")
+                navigation.selectedItemId = R.id.navigationSearch
+            }
+            intent.hasExtra("fragmentAboutUs") -> {
+                fragmentid = intent.getStringExtra("fragmentAboutUs")
+                navigation.selectedItemId = R.id.navigationAboutUs
+            }
+        }
+
     }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener {
+            menuItem ->
+        when (menuItem.itemId){
+
+            R.id.navigationFavorites -> {
+                val fragment = FavoriteFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment,
+                    fragment.javaClass.simpleName).commit()
+
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigationSearch -> {
+                val fragment = SearchFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, fragment,
+                    fragment.javaClass.simpleName).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.navigationAboutUs -> {
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+
+    }
+
+
+
 }
